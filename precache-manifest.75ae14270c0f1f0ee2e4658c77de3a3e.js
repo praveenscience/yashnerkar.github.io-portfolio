121401c74fa24710eb8c2a183472ec2f541ec492
yash-png <yashnerkar512@gmail.com>
2022-08-11 23:37:12 +0530

    Updates

--- precache-manifest.75ae14270c0f1f0ee2e4658c77de3a3e.js
+++ precache-manifest.75ae14270c0f1f0ee2e4658c77de3a3e.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "79535cc886ff14a74f3b",
+    "revision": "3eb96c0ed89cb735b12b",
     "url": "/static/css/main.26657a8b.chunk.css"
   },
   {
-    "revision": "79535cc886ff14a74f3b",
-    "url": "/static/js/main.79535cc8.chunk.js"
+    "revision": "3eb96c0ed89cb735b12b",
+    "url": "/static/js/main.3eb96c0e.chunk.js"
   },
   {
     "revision": "11c4aba4207efa4b42e3",
     "url": "/static/js/1.11c4aba4.chunk.js"
   },
   {
-    "revision": "95452026201717503e0b",
-    "url": "/static/js/2.95452026.chunk.js"
+    "revision": "f97cb5a208a9c34caf52",
+    "url": "/static/js/2.f97cb5a2.chunk.js"
   },
   {
     "revision": "2397c5b165d0c8a14c8a",
     "url": "/static/js/runtime~main.2397c5b1.js"
   },
   {
-    "revision": "d3496bc7a5ae69a3d74ad49e099dea55",
+    "revision": "e3aa01a8ae606e04b72571fef8e81f7d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0073597614edb1ba694f52fa868385d2.js"
+  "/precache-manifest.75ae14270c0f1f0ee2e4658c77de3a3e.js"
 );
 
 workbox.clientsClaim();
